54b50913ae510b0b9bc98ab1d7fdd5e9b24ef363
aabdullahsayed <abdullahalsayed087@gmail.com>
2025-04-11 23:56:44 +0600

    Random Source code

---
+++ bruh.java
@@ -0,0 +1,27 @@
+package nerdapple;
+import java.util.Scanner;
+
+public class bruh
+{
+	public static void main(String[] args)
+	
+	{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("This is my first java code, Enter Your Name : ");
+		String name = input.nextLine();
+		
+		System.out.pri#include<iostream>
+		using namespace std;
+		int main()
+		{
+		int a,b,c,d;
+		cin>>a>>b>>c>>d;
+		cout<<"DIFERENCA = "<<a*b-c*d<<endl;
+		return 0 ;
+		}
+ntln("The user name is "+name);
+		input.close();
+		
+	}
+}
